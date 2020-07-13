0406a7ddeaae1e56b3e2cc387519f7dec4bedb43
Sori <sori@ip-172-26-11-178.ap-northeast-2.compute.internal>
2020-07-13 11:31:37 +0900

    Free buf

--- add-nbo.cpp
+++ add-nbo.cpp
@@ -18,6 +18,7 @@ uint32_t fpToUint32(FILE* fp){
 	}
 	uint32_t* p = reinterpret_cast<uint32_t*>(n);
 	uint32_t result = htonl(*p);
+	free(buf);
 	return result;
 }
 int main(int argc,char* argv[]){
@@ -36,5 +37,7 @@ int main(int argc,char* argv[]){
 	
 	printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n",num1,num1,num2,num2,num1+num2,num1+num2);
 
+	fclose(fp1);
+	fclose(fp2);
 	return 0;
 }
